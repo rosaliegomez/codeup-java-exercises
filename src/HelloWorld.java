32eb0cd440b3705fd2a363a69f3178f8ee17c70c
rosaliework <rosalie.gomez12@gmail.com>
2017-09-18 09:59:56 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    //entry point
+    public static void main(String[] args) {
+        //sout -> shortcut for system.out.println();
+        System.out.print("Hello world!");
+    }
+}
